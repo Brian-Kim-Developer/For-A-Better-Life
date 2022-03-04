f596804c7694dac1c61706d2a67cd7c4283c1dcd
Gyeongwon Kim <kkwysy7963@gmail.com>
2022-03-04 21:30:07 +0900

    Deployment setting 2

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: '',
+  baseURL: 'https://quotes.rest',
   headers: {
     'accept': 'application/json',
     'X-TheySaidSo-Api-Secret': `${process.env.REACT_APP_API_KEY}`
